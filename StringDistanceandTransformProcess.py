56fcf80fbdac78091f853e2c26e5b1a9f49a4eec
arieldg1997 <37058073+arieldg1997@users.noreply.github.com>
2020-10-20 15:58:25 -0300

    Add files via upload

--- StringDistanceandTransformProcess.py
+++ StringDistanceandTransformProcess.py
@@ -1,8 +1,5 @@
 from sys import stdin, stdout
 
-
-
-
 def edist(l1,l2): 
     n=len(l1)+1
     m=len(l2)+1
@@ -12,53 +9,42 @@ def edist(l1,l2):
             delete=memo[i-1][j] + 1
             insert=memo[i][j-1] + 1 
             memo[i][j] = min(replace,delete,insert) 
+    traza(n-1,m-1,l1,l2)
     return memo[n-1][m-1]
 
 
-def traza():
-    n=len(l1)+1
-    m=len(l2)+1
-    stdout.write(f.'{}\n',memo[n][m])
-    int step = 0, i = n, j = m;
-    while (i || j) {
-        if (x[i - 1] == y[j - 1]) {
-            i--, j--;
-            continue;
-        }
-        printf("%d ", ++step);
-        if (i > 0 && f[i][j] == f[i - 1][j] + 1) { // delete
-            printf("Delete %d", i);
-            x.erase(i - 1, 1);
-            i--;
-        }
-        else if (j > 0 && f[i][j] == f[i][j - 1] + 1) { // insert
-            printf("Insert %d,%c", i + 1, y[j - 1]);
-            x.insert(i, 1, y[j - 1]);
-            j--;
-        }
-        else if (i > 0 && j > 0) {  // replace
-            printf("Replace %d,%c", i, y[j - 1]);
-            x[i - 1] = y[j - 1];
-            i--, j--;
-        }
-        putchar('\n');
-    }
-}
+def traza(i,j,l1,l2):
+    while i>0 or j>0:
+        if i>0 and j>0 and l1[i-1] == l2[j-1]:
+            i-=1
+            j-=1
+        elif i>0 and memo[i][j] == memo[i-1][j] +1:
+            operations.insert(0,("Delete",i,"\n"))
+            i-=1
+        elif j>0 and memo[i][j] == memo[i][j-1] +1:
+            operations.insert(0,("Insert",i+1,","+l2[j-1]+"\n"))
+            j-=1
+        elif i>0 and j>0 and l1[i-1] != l2[j-1]:
+            operations.insert(0,("Replace",i,","+l2[j-1]+"\n"))
+            i-=1
+            j-=1
 
 memo=[[-1 for _ in range(100)] for _ in range(100)]
 for i in range(100): memo[i][0] = i 
 for j in range(100): memo[0][j] = j  
 
-
-
-
-
-
-
-
-while True:
-    l1=stdin.readline().strip()
+buliano=True
+for line in stdin:
+    l1=line.strip()
     l2=stdin.readline().strip()
-    
-    stdout.write(str(edist(l1,l2))+"\n")
-    
+    if buliano==False: stdout.write("\n")
+    else: buliano=False
+    operations=[]
+    n=edist(l1,l2)
+    stdout.write(str(n)+"\n")
+    sesgo=0
+    for elem in range(n):
+        aux=operations[elem]
+        stdout.write(str(elem+1)+" "+aux[0]+" "+str(aux[1]+sesgo)+aux[2])
+        if operations[elem][0]=="Delete": sesgo-=1
+        elif operations[elem][0]=="Insert": sesgo+=1
